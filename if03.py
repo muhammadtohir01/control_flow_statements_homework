882e09b14114ae0073adf3317dec9635421aef94
muhammadtohir01 <nabiyevmuhammadtohir@gmail.com>
2022-10-18 23:21:04 +0500

    3 misol

--- if03.py
+++ if03.py
@@ -1,3 +1,6 @@
+import re
+
+
 def main(a):
     """
     If the number is positive, increase it to 1, else decrease it to 2. If it is 0, assign 10.
@@ -6,4 +9,10 @@ def main(a):
     Returns:
         a: integer
     """
-    return
+    if a>0:
+        return a+1
+    if a==0:
+        return 10
+    else: 
+     return a-2 
+print(main(0))
