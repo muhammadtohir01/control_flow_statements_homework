7fde3edcc812ee6982f110dfc8fefe18abf34ed9
muhammadtohir01 <nabiyevmuhammadtohir@gmail.com>
2022-10-19 13:53:57 +0500

    4 misol

--- if04.py
+++ if04.py
@@ -8,4 +8,14 @@ def main(a,b,c):
     returns:
         integer: the number of positive numbers in the given numbers
     """
-    return
+    x1=0
+    x2=0
+    x3=0
+    if a>0:
+        x1=1
+    if b>0:
+        x2=1
+    if c>0:
+        x3=1
+    return x1+x2+x3
+print(main(2,-2,9))
